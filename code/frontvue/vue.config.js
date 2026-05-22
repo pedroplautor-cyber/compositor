0b92b9229a0a5b12dcb81257072bf63c7b66ddf3
pedroplautor-cyber <pedropl.autor@gmail.com>
2026-05-22 11:35:24 +0200

    fix en despliegue

--- code/frontvue/vue.config.js
+++ code/frontvue/vue.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/compositor/'
+    : '/',
+
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
 })
